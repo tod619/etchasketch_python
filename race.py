82fb553cf45d28ede5d952810e4f01280ff3ad2f
tod619 <trevorodwyer619@hotmail.com>
2023-06-18 20:03:09 +0100

    Create 6 turtles with diffent colours and positions on the y axis in race.py file

--- race.py
+++ race.py
@@ -3,9 +3,13 @@ screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet!",
                             prompt="Which turtle will win the race? Pick a colour: ")
-print(user_bet)
+colors = ["red", "orange", "yellow", "blue", "green", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
 
-tim = Turtle(shape="turtle")
-tim.penup()
-tim.goto(x=-230, y=-100)
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(f"{colors[turtle_index]}")
+    tim.penup()
+    tim.goto(x=-230, y=y_positions[turtle_index])
+
 screen.exitonclick()
