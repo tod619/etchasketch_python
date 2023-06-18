87dbe8d09584b02c9e25cef1e093b862971b1980
tod619 <trevorodwyer619@hotmail.com>
2023-06-18 17:35:18 +0100

    Add onkey events on w, s, a, d keys in main.py file

--- main.py
+++ main.py
@@ -24,7 +24,10 @@ def turn_right():
 
 
 screen.listen()
-screen.onkey(key="space", fun=turn_left)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
 
 
 screen.exitonclick()
