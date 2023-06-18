4cb5ae502932394953f6715c03c1cd7b5596ed8e
tod619 <trevorodwyer619@hotmail.com>
2023-06-18 17:48:04 +0100

    Create clear function in main.py file

--- main.py
+++ main.py
@@ -25,11 +25,19 @@ def turn_right():
     tim.setheading(new_heading)
 
 
+def clear():
+    tim.penup()
+    tim.clear()
+    tim.home()
+    tim.pendown()
+
+
 screen.listen()
 screen.onkey(key="w", fun=move_forwards)
 screen.onkey(key="s", fun=move_backwards)
 screen.onkey(key="a", fun=turn_left)
 screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear)
 
 
 screen.exitonclick()
