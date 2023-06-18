631996755e2ef1e4082fe1c2840bdb3e79dbb1e2
tod619 <trevorodwyer619@hotmail.com>
2023-06-18 19:49:58 +0100

    create turtle + add shape + move to starting line in race.py file

--- race.py
+++ race.py
@@ -1,8 +1,11 @@
 from turtle import Turtle, Screen
-tim = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet!",
                             prompt="Which turtle will win the race? Pick a colour: ")
 print(user_bet)
+
+tim = Turtle(shape="turtle")
+tim.penup()
+tim.goto(x=-230, y=-100)
 screen.exitonclick()
